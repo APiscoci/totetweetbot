2d764d61ba6d81ee1297348f4867a08fd3e4246b
AncaPiscoci <anca.piscoci@tote.co.uk>
2020-06-04 12:19:58 +0100

    add all files

--- bots.js
+++ bots.js
@@ -47,28 +47,3 @@ tweetBot.get('search/tweets', favParams, (err, data, res) => {
     console.log(err);
   }
 });
-
-// const twitterApi = require('./touch');
-
-// const Bot = async (event, context, callback) => {
-//   const { text, image } = event;
-
-//   if (!text) {
-//     console.log('No text given for tweet. event: ', event);
-//     return false;
-//   }
-
-//   const { exception, url } = await twitterApi.postStatus(text, image);
-
-//   callback(exception, {
-//     body: {
-//       url,
-//     },
-//     headers: {
-//       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-//     },
-//   });
-// };
-// const event = { text: 'aaa', image: null };
-
-// Bot(event);
